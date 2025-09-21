f28eb411f862421e36b11f93f3116bec2414565a
Aaron1803 <73663116+Aaron75A@users.noreply.github.com>
2025-09-20 19:27:29 -0500

    main.py

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 def mostrar_menu():
-    print("\n--- SISTEMA DE GESTIÓN DE SERVICIOS ---")
-    print("1. Ingresar reserva")
-    print("2. Editar reserva")
-    print("3. Cancelar reserva")
-    print("4. Consultar reservas")
-    print("5. Ingresar especialista/trabajador")
-    print("6. Editar especialista/trabajador")
-    print("7. Borrar especialista/trabajador")
-    print("8. Ingresar servicio")
-    print("9. Editar servicio")
-    print("10. Borrar servicio")
-    print("0. Salir")
-
-def ejecutar_menu():
     while True:
-        mostrar_menu()
-        opcion = input("Seleccione una opción: ")
 
-        if opcion == "1":
-            '''print()
-        elif opcion == "2":
+        print("\n--- SISTEMA DE GESTIÓN DE SERVICIOS ---")
+        print("1. Ingresar reserva")
+        print("2. Editar reserva")
+        print("3. Cancelar reserva")
+        print("4. Consultar reservas")
+        print("5. Ingresar especialista/trabajador")
+        print("6. Editar especialista/trabajador")
+        print("7. Borrar especialista/trabajador")
+        print("8. Ingresar servicio")
+        print("9. Editar servicio")
+        print("10. Borrar servicio")
+        print("0. Salir")
+    
+        opcion = input("Seleccione una opción: ")
+        if opcion == 1:
+            print("hola mundo")
+
+        elif opcion == 2:
             print()
-        elif opcion == "3":
+        elif opcion == 3:
             print()
-        elif opcion == "4":
+        elif opcion == 4:
             print()
-        elif opcion == "5":
+        elif opcion == 5:
             print()
-        elif opcion == "6":
+        elif opcion == 6:
             print()
-        elif opcion == "7":
+        elif opcion == 7:
             print()
-        elif opcion == "8":
+        elif opcion == 8:
             print()
-        elif opcion == "9":
+        elif opcion == 9:
             print()
-        elif opcion == "10":
+        elif opcion == 10:
             print()
-        elif opcion == "0":
+        elif opcion == 0:
             print()
             break
         else:
             print("Opción inválida. Intente de nuevo.")
-'''
-# Este seria el menu base desde donde partiremos y aunque se vea mas tedioso pues es con lo que mejor se trabajar, hay que crear funciones y listas con los especialistas y los servicios
+# Ejecutar el menú
+mostrar_menu()
 
-ejecutar_menu()
+
