c3386026aa0123c14e9ef3b381bdec89d62166cd
berserk2046 <berserkiceland@protonmail.com>
2025-09-24 00:11:51 -0500

    Funcion para particion de horarios, creacion de citas y disponibilidad

--- main.py
+++ main.py
@@ -1,23 +1,75 @@
-# Notas (TODO): Convencion de nombre especialista/empleado en funciones relacionadas con estos.
+# Notas:
+#(TODO): 
+# 1. Convencion de nombre especialista/empleado en funciones relacionadas con estos.
+# 2. Mejorar el formato de la toma de la duracion del servicio (1:30 hr por ejemplo no se puede con el sistema actual.)
 
 # Lista que contendra todos los servicios que declaremos para el sistema.
 servicios = []
 
 #Servicios predefinidos para que no inicie el programa vacio.
-servicio1 = [{'Nombre':'x', 'Duracion':10, 'Costo': 2000}, {'Empleados':[]}]
-servicio2 = [{'Nombre':'y', 'Duracion':30, 'Costo': 15000}, {'Empleados':[]}]
+#IDuracion es la parte numerica de la duracion, mientras que SDuracion es la parte en texto, se usa por accesibilidad.
+servicio1 = [{'Nombre':'x', 'IDuracion':20, 'SDuracion': "min", 'Costo': 2000}, {'Empleados':[]}]
+servicio2 = [{'Nombre':'y', 'IDuracion':30, 'SDuracion': "min", 'Costo': 15000}, {'Empleados':[]}]
 
 # Los servicios se anaden como copias a la lista para evitar errores con la memoria del programa
 # Por ejemplo, si anado dos veces servicio1 y modifico informacion de este se modificara en las dos copias que anadi, usar .copy() evita esto.
 servicios.append(servicio1.copy())
 servicios.append(servicio2.copy())
-print(servicios)
+
+#convierte string en formato "10:00-12:20" en una lista de la forma [10.00, 12.20]
+def horario_empleado(x):
+    x = x.split('-')
+    inicio = x[0]
+    final = x[1]
+    inicio = inicio.replace(':', '.')
+    final = final.replace(':', '.')
+    inicio = float(inicio)
+    final = float(final)
+    return [inicio, final]
+
+#NumeroString_split
+def ns_split(x):
+    sduracion = ""
+    for i in x:
+        if(i.isdigit() == False):
+            sduracion += i
+            x = x.replace(i, "") #Elimina el caracter no entero de la variable
+    # esto retornaria por eje [30, 'min']
+    return [int(x), sduracion]
+
+def creacion_citas(info_servicio, horario):
+    duracion = 0
+    ultima_jornada = horario[0] * 60
+    lista_horas = []
+    if(info_servicio['SDuracion'] == "hr"): duracion = info_servicio['IDuracion'] * 60
+    elif(info_servicio['SDuracion'] == "min"): duracion = info_servicio['IDuracion']
+    tiempo_jornada = (horario[1]*60) - (horario[0]*60)
+    for i in range(0, int(tiempo_jornada / duracion)):
+        x = [ultima_jornada, ultima_jornada+duracion]
+        ultima_jornada = x[1]
+        x[0] /= 60
+        x[1] /= 60
+        x1_minutos = (x[0] - int(x[0])) * 60
+        x2_minutos = (x[1] - int(x[1])) * 60
+        if(int(x1_minutos)== 0): x1_minutos = "00"
+        else: x1_minutos = str(int(round(x1_minutos)))
+        if(int(x2_minutos)== 0): x2_minutos = "00"
+        else: x2_minutos = str(int(round(x2_minutos)))
+        
+        x[0] = str(int(x[0])) + ":" + x1_minutos
+        x[1] = str(int(x[1])) + ":" + x2_minutos
+        lista_horas.append(x)
+    return lista_horas
+
+        
+
 
 def crear_servicio():
     nombre = input("Ingrese el nombre del servicio: ")
-    duracion = int(input("Ingrese la duracion del servicio: "))
+    duracion = input("Ingrese la duracion del servicio (formato: numero-(hr/min/s), eje: 30min, 1hr, 20s): ")
+    duracion = ns_split(duracion)
     costo = int(input("Ingrese el costo del servicio: "))
-    servicio = [{'Nombre': nombre, 'Duracion': duracion, 'Costo': costo}, {'Empleados':[]}]
+    servicio = [{'Nombre': nombre, 'IDuracion': duracion[0], 'SDuracion': duracion[1], 'Costo': costo}, {'Empleados':[]}]
     servicios.append(servicio.copy()) #Usamos append con un servicio.copy para que usar un memory address diferente y poder hacer modificaciones a gusto sin que se este cambiando siempre el mismo servicio.
 
 def modificar_servicio():
@@ -31,10 +83,12 @@ def modificar_servicio():
         nuevo_nombre = input("Ingrese el nuevo nombre del servicio: ")
         servicios[x][0]['Nombre'] = nuevo_nombre
     if(variable.lower() == "duracion"):
-        nueva_duracion = input("Ingrese la nueva duracion del servicio: ")
-        servicios[x][0]['Duracion'] = nueva_duracion
+        nueva_duracion = input("Ingrese la nueva duracion del servicio (formato: numero-(hr/min/s), eje: 30min, 1hr, 20s): ")
+        nueva_duracion = ns_split(nueva_duracion)
+        servicios[x][0]['IDuracion'] = nueva_duracion[0]
+        servicios[x][0]['SDuracion'] = nueva_duracion[1]
     if(variable.lower() == "costo"):
-        nuevo_costo = input("Ingrese el nuevo costo del servicio: ")
+        nuevo_costo = int(input("Ingrese el nuevo costo del servicio: "))
         servicios[x][0]['Costo'] = nuevo_costo
     else:
         print("La variable que usted quiere modificar no existe, por favor intente de nuevo.")
@@ -61,7 +115,7 @@ def informacion_servicio():
         print("El servicio que usted eligio no existe, intentelo de nuevo.\n")
     else:
         print(f"\nNombre: {servicios[x][0]['Nombre']}")
-        print(f"Duracion: {servicios[x][0]['Duracion']}")
+        print(f"Duracion: {servicios[x][0]['IDuracion']}{servicios[x][0]['SDuracion']}")
         print(f"Costo: {servicios[x][0]['Costo']}")
         print(f"Cantidad de empleados: {len(servicios[x][1]['Empleados'])}")
         if(len(servicios[x][1]['Empleados']) == 0):
@@ -77,6 +131,9 @@ def informacion_servicio():
                 print(f"Celular: {servicios[x][1]['Empleados'][i]['Cel']}")
                 print(f"Email: {servicios[x][1]['Empleados'][i]['Email']}")
                 print(f"Horario: {servicios[x][1]['Empleados'][i]['Horario']}\n")
+                print(f"Disponibilidad (Horas en las que admite cita.): ")
+                for a in range(0, len(servicios[x][1]['Empleados'][i]['Disponibilidad'])):
+                    print(f"[{a+1}] {servicios[x][1]['Empleados'][i]['Disponibilidad'][a]}")
 
 def ingresar_especialista():
     print("A que servicio desea anadir un especialista?: \n")
@@ -93,7 +150,9 @@ def ingresar_especialista():
         cedula = int(input("Cedula: "))
         cel = int(input("Celular: "))
         email = input("Email: ")
-        horario = input("Horario (Ingreselo como inicio(am/pm)-final(am/pm)): ")
+        horario = input("Horario (Se usa formato 24hr y separacion por '-', eje: 8:00-15:20): ")
+        horas = horario_empleado(horario)
+        lista_horas = creacion_citas(servicios[x][0], horas)
 
         empleado = {
             'Nombre': nombre,
@@ -101,7 +160,8 @@ def ingresar_especialista():
             'Cedula': cedula,
             'Cel': cel,
             'Email': email,
-            'Horario': horario,
+            'Disponibilidad': lista_horas,
+            'Horario': horario
             }
 
         servicios[x][1]['Empleados'].append(empleado.copy())
@@ -129,7 +189,11 @@ def modificar_especialista():
         servicios[x][1]['Empleados'][y]['Cedula'] = int(input("Nuevo No. cedula: "))
         servicios[x][1]['Empleados'][y]['Cel'] = int(input("Nuevo No. Cel: "))
         servicios[x][1]['Empleados'][y]['Email'] = input("Nuevo Email: ")
-        servicios[x][1]['Empleados'][y]['Horario'] = input("Nuevo Horario (Ingreselo como inicio(am/pm)-final(am/pm)): ")
+        servicios[x][1]['Empleados'][y]['Horario'] = input("Nuevo Horario (Se usa formato 24hr y separacion por '-', eje: 8:00-15:20): ")
+        horas = horario_empleado(servicios[x][1]['Empleados'][y]['Horario'])
+        lista_horas = creacion_citas(servicios[x][0], horas)
+        servicios[x][1]['Empleados'][y]['Disponibilidad'] = lista_horas
+
         print(f"La informacion de {servicios[x][1]['Empleados'][y]['Nombre']} ha sido modificada con exito.\n")
     elif(tipo_modificacion.lower() == "especifica"):
         variable = input("Que variable desea modificar? (cedula, celular, email u horario): ")
@@ -143,7 +207,10 @@ def modificar_especialista():
             servicios[x][1]['Empleados'][y]['Email'] = input("Nuevo Email: ")
             print(f"El Email de {servicios[x][1]['Empleados'][y]['Nombre']} ha sido modificado con exito.\n")
         if(variable.lower() == "horario"):
-            servicios[x][1]['Empleados'][y]['Horario'] = input("Nuevo Horario (Ingreselo como inicio(am/pm)-final(am/pm)): ")
+            servicios[x][1]['Empleados'][y]['Horario'] = input("Nuevo Horario (Se usa formato 24hr y separacion por '-', eje: 8:00-15:20): ")
+            horas = horario_empleado(servicios[x][1]['Empleados'][y]['Horario'])
+            lista_horas = creacion_citas(servicios[x][0], horas)
+            servicios[x][1]['Empleados'][y]['Disponibilidad'] = lista_horas
             print(f"El horario de {servicios[x][1]['Empleados'][y]['Nombre']} ha sido modificado con exito.\n")
 
 
@@ -177,8 +244,9 @@ empleado1 = {
         'Cedula': '10000000',
         'Cel':'3004929192',
         'Email':'pepito@hotmail.com',
-        'Horario': '9am-5pm',
-        'Reservas': [{'Cliente': 'x', 'Fecha': 'Martes', 'Hora': 20}] # Solo se aceptan reservas dentro del horario, si la lista esta vacia se puede borrar servicio/empleado.
+        'Horario': '9:00-17:00',
+        'Disponibilidad': creacion_citas(servicios[0][0], horario_empleado('9:00-17:00')),
+        'Reservas': [{'Cliente': 'x', 'Fecha': '08/9/23', 'Hora': 20}] # Solo se aceptan reservas dentro del horario, si la lista esta vacia se puede borrar servicio/empleado.
         }
 
 
@@ -197,12 +265,14 @@ usuario = {
 # Funcion imcompleta, no se debe de usar.
 def anadir_reserva():
     for i in range(0, len(servicios)):
-        print(f"[{i}] {servicios[i][0]['Nombre']}\n")
+        print(f"[{i+1}] {servicios[i][0]['Nombre']}\n")
 
     servicio = int(input("Que servicio desea: "))
+    servicio -= 1
     for i in range(0, len(servicios[servicio][1]['Empleados'])):
-        print(f"[{i}] Empleado: {servicios[servicio][1]['Empleados'][i]}")
+        print(f"[{i+1}] Empleado: {servicios[servicio][1]['Empleados'][i]}")
     empleado = (int(input("Seleccione el empleado que desea: ")))
+    empleado -= 1
     fecha = input("En que fecha lo desea: ")
     hora = input("En que hora lo desea: ")
     reserva_empleado = {'Cliente': usuario['Nombre'], 'Fecha':fecha, 'Hora':hora}
